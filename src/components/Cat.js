f57f0f34257c7c3595e54b43d9afd8300adc2113
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-02 10:44:18 +0100

    query is done

--- src/components/Cat.js
+++ src/components/Cat.js
@@ -1,14 +1,11 @@
-import {useQuery} from "@tanstack/react-query";
-import Axios from "axios";
-
+import {useGetCat} from "../useGetCat";
 export const Cat = () => {
-    const { data: catData } = useQuery(["cat"], async () => {
-        return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
-    });
-
+    const {data, isCatLoading, refetchData} = useGetCat();
+    if(isCatLoading) return <h1> loading...</h1>
     return (
         <div>
-            <h1> {catData?.fact} </h1>
+            <button onClick={refetchData}> refetch </button>
+            <h1> {data?.fact} </h1>
         </div>
     )
 }
