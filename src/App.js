b47bc1af505656b783120ad4521dc903ab5c0f4b
Jiali Zhang <zhangjiali86@gmail.com>
2022-11-03 14:51:06 +0100

    useCount finished

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import './App.css';
-import {Cat} from "./components/Cat"; 
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {useCount} from "./components/useCount"
 
 function App() {
-    const client =new QueryClient({
-        defaultOptions: {
-            queries:{
-                refetchOnwindowFocus: true,
-            },
-        },
-    });
+    const {count, decrease, increase, reset} = useCount;
   return (
     <div className="App">
-        <QueryClientProvider client={client}>
-        <Cat/>
-        </QueryClientProvider>        
+        {count}
+       <button onClick={decrease}>decrease</button>
+       <button onClick={increase}>increase</button>
+       <button onClick={reset}>reset</button>
+
     </div>
   );
 }
